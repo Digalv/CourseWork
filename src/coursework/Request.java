cc16cafc114bf84fcfe46661e05e1a0933142fc7
Digalv <daniil.tabakov004@gmail.com>
2022-06-11 13:45:16 +0200

    Fixed

--- src/coursework/Request.java
+++ src/coursework/Request.java
@@ -3,7 +3,9 @@ package coursework;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Класс для обработки запросов с клиента
@@ -24,16 +26,27 @@ public class Request {
             return "{\"Incorrect\": \"1\"}";
         }
     }
-    public static String delete(String ListJSON) throws IOException {
+    public static String delete(String listJSON) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        Main.getCountries().setCountriesList(objectMapper.readValue(ListJSON, ArrayList.class));
+        Main.getCountries().setCountriesList(objectMapper.readValue(listJSON, ArrayList.class));
         DataBase.SaveJSON(Main.getCountries().getCountriesList());
         return "{\"Correct\": \"1\"}";
     }
-    public static String edit(String ListJSON) throws IOException {
+
+    public static String edit(String string) throws IOException {
+        String[] response = string.split(",\\{");
+        int id = Integer.parseInt(response[0]);
         ObjectMapper objectMapper = new ObjectMapper();
-        Main.getCountries().setCountriesList(objectMapper.readValue(ListJSON, ArrayList.class));
-        DataBase.SaveJSON(Main.getCountries().getCountriesList());
-        return "{\"Correct\": \"1\"}";
+        Country country = objectMapper.readValue("{" + response[1], Country.class);
+        if(Country.correctCountry(country)){
+            System.out.println("correct");
+            Main.getCountries().getCountriesList().set(id, country);
+
+            DataBase.SaveJSON(Main.getCountries().getCountriesList());
+            return "{\"Correct\": \"1\"}";
+        }
+        else{
+            return "{\"Incorrect\": \"1\"}";
+        }
     }
 }
